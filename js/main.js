2859d4c9b1f870aeca0255b82911e56c127727f4
Christie <r_christie3@tss-hr420-dm30.local>
2017-03-03 15:57:20 -0500

    march 3rd class files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 (function() {
 	//i dont work in crome
-	var map = new google.maps.Map(document.querySelector('.mapWraper')), marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')),
+	preloader = document.querySelector('.preload-wrapper'),
+
+	// import geocode api
+	geocoder = new google.maps.Geocoder(),
+	geocodeButton = document.querySelector('.geocode'),
+
+	//directions
+	directionsServices = new google.maps.DirectionsService(),
+	directionsDisplay,
+	locations = [],
+
+	marker;
 
 	function initMap(position){
+
+		//save our location
+		locations[0] = { lat: position.coords.latitude, lng: position.coords.longitude };
+
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		directionsDisplay.setMap(map);
+
 		map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
 			map.setZoom(14);
 			
@@ -11,6 +30,7 @@
 				map : map,
 				title : "hello world"
 		});
+		preloader.classList.add('hide-preloader');
 		//add custom animation for the marker 
 	}
 	if (navigator.geolocation){
@@ -18,7 +38,46 @@
 	}else{
 		console.log("nope");
 	}
+
 	function handleError(){
 		console.log('somthing went wrong');
 	}
+//add turn by turn directions
+	function codeAddress(){
+		var address = document.querySelector('.address').value;
+		geocoder.geocode({'address': address}, function(results, status){
+			if(status == google.maps.GeocoderStatus.OK){
+				// push location into array
+				locations[1] = { lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng() }
+				map.setCenter(results[0].geometry.location);
+				if(marker){
+					marker.setMap(null);
+					marker = new google.maps.Marker({
+						map : map,
+						position : results[0].geometry.location
+					});
+
+				calcRoute(results[0].geometry.location);
+
+				} else {
+					console,log(status);
+				}
+			}
+		});
+	}
+
+	function calcRoute(codedLoc){
+		var request = {
+			origin: locations[0],
+			destination: locations[1],
+			travelMode: "DRIVING"
+		};
+
+		directionsServices.route(request, function(responce, status){
+			if (status == "OK"){
+				directionsDisplay.setDirections(responce);
+			}
+		});
+	}
+	geocodeButton.addEventListener('click',codeAddress,false);
 })();
